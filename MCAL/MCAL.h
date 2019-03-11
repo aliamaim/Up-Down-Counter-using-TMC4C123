92b9342f6c4ebf3d3b74b78bf15162ee46bbbbd0
aliamaim <ali.amaim@yahoo.com>
2019-03-12 01:01:21 +0200

    Final MCAL.h version, hopefully no bugs rise

--- MCAL/MCAL.h
+++ MCAL/MCAL.h
@@ -1,8 +1,10 @@
+#ifndef MCAL_HEADER
+#define MCAL_HEADER
+
 #include "std_types.h"
 
 
-/* Port Indexes */
-
+/* PORT INDEXES */
 #define PORTA (0u)
 #define PORTB (1u)
 #define PORTC (2u)
@@ -10,13 +12,17 @@
 #define PORTE (4u)
 #define PORTF (5u)
 
-/* Enumerations */
+/* PULL-UP PULL-DOWN */
+#define DISABLE (0u)
+#define ENABLE (1u)
+
+/* ENUMBERATIONS */
 enum Port_PinDirectionType{PORT_PIN_IN, PORT_PIN_OUT};
 
 enum Dio_LevelType{STD_LOW, STD_HIGH};
 
 
-/* Port Driver */
+/* PORT DRIVER */
 
 /*
  * Initialize port based on the port_index
@@ -40,7 +46,55 @@ void Port_Init(uint8 port_index);
 void Port_SetPinDirection(uint8 port_index, uint8 pins_mask, Port_PinDirectionType pins_direction);
 
 
+/*
+ * If enable is 1 (ENABLE), the selected pins by pins_mask in the
+ * port selected by port_index will be connected to internal pull-up resistor
+ * if enable is 0 (DISABLE, the selected pins will not be connected to the internal
+ * pull-up resistor.
+ * Example: Port_SetPinPullUp(PORTB, 0x00000003, ENABLE);
+ * this example would make pin number 0, 1 connected to the internal pull-up resistor
+ */
+void Port_SetPinPullUp(uint8 port_index, uint8 pins_mask, uint8 enable);
 
 
+/*
+ * If enable is 1 (ENABLE), the selected pins by pins_mask in the
+ * port selected by port_index will be connected to internal pull-down resistor
+ * if enable is 0 (DISABLE), the selected pins will not be connected to the internal
+ * pull-down resistor.
+ * Example: Port_SetPinPullDown(PORTB, 0x00000003, ENABLE);
+ * this example would make pin number 0, 1 connected to the internal pull-down resistor
+ */
+void Port_SetPinPullDown(uint8 port_index, uint8 pins_mask, uint8 enable);
 
-/* DIO Driver */
+
+/* DIO DRIVER */
+
+/*
+ * Return the value of the PINS selected by pins_mask in the
+ * port selected by port_index
+ * Example: DIO_ReadPort(PORTC, 0x00000003);
+ * this example would read pins 0 & 1 in portc
+ * if the value returned is:
+ * 0 --> both pins are 0
+ * 1 --> PIN0 is 1, PIN1 is 0
+ * 3 --> PIN0 & PIN1 are 1
+ */
+uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask);
+
+
+/*
+ * Change the value of the pins selected by pins_masks in the
+ * port selected by port_index to pins_level
+ * Example: DIO_WritePort(PORTE, 0x00000005, STD_HIGH);
+ * this example would set pin0 & pin2 to HIGH
+ */
+void DIO_WritePort(uint8 port_index, uint8 pins_mask, Dio_LevelType pins_level);
+
+/*
+ * Toggle the values of the pins selected by pins_masks in the
+ * port selected by port_index
+ */
+void DIO_FlipPort(uint8 port_index, uint8 pins_mask);
+
+#endif
