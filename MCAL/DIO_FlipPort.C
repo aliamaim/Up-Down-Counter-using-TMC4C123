dee849f22236d270df8074becaec415d54aaec86
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-03-16 05:13:46 +0200

    Create DIO_FlipPort.C

---
+++ MCAL/DIO_FlipPort.C
@@ -0,0 +1,13 @@
+void DIO_FlipPort(uint8 port_index ,uint8 pins_mask){
+	
+	switch(port_index){
+	case PORTA: { GPIO_PORTA_DATA_R^=pins_mask; }
+	case PORTB:	{ GPIO_PORTB_DATA_R^=pins_mask; }
+	case PORTC:	{ GPIO_PORTC_DATA_R^=pins_mask; }
+	case PORTD:	{ GPIO_PORTD_DATA_R^=pins_mask; }
+	case PORTE:	{ GPIO_PORTE_DATA_R^=pins_mask; }
+	case PORTF:	{ GPIO_PORTF_DATA_R^=pins_mask; }
+	
+}
+
+
