c74b2ec577d23eea3b178951a083fe68f3abdd94
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-03-17 02:13:59 -0700

    Update DIO_WritePort.c

--- MCAL/DIO_WritePort.c
+++ MCAL/DIO_WritePort.c
@@ -6,46 +6,109 @@ void DIO_WritePort(uint8 port_index , uint8 pins_mask , Dio_LevelType pins_level
 {
    switch(port_index){
 	               case PORTA:
+		    {
                              switch(pins_level):
+			    {
                                                case STD_LOW:
+				               
                                                         GPIO_PORTA_DATA_R=(GPIO_PORTA_DATA_R &~ pins_mask);
+		                                        break;
+					       
                                                case STD_HIGH:
+				               
                                                         GPIO_PORTA_DATA_R=(GPIO_PORTA_DATA_R | pins_mask);
-                                   
+                                                        break;
+					       
+			     }
+		                               break;
+		      }
 	               case PORTB:
+		   {
                              switch(pins_level):
+			   {
                                                case STD_LOW:
+				              
                                                         GPIO_PORTB_DATA_R=(GPIO_PORTB_DATA_R &~ pins_mask);
+		                                        break;
+				              
                                                case STD_HIGH:
+				              
                                                         GPIO_PORTB_DATA_R=(GPIO_PORTB_DATA_R | pins_mask);
-                                   
+					                break;
+				              
+				   
+			   }
+			   break;
+		   }     
 	               case PORTC:
+		   {
                              switch(pins_level):
+			   {
                                                case STD_LOW:
+				              
                                                         GPIO_PORTC_DATA_R=(GPIO_PORTC_DATA_R &~ pins_mask);
+		                                        break;
+				              
                                                case STD_HIGH:
+				              
                                                         GPIO_PORTC_DATA_R=(GPIO_PORTC_DATA_R | pins_mask);
-                                    
+					                break;
+				              
+			   }
+			   break;
+		   }
 	               case PORTD:
+		   {
                              switch(pins_level):
+			   {
                                                case STD_LOW:
+				               
                                                         GPIO_PORTD_DATA_R=(GPIO_PORTD_DATA_R &~ pins_mask);
+						        break;
+					       
                                                case STD_HIGH:
+				               
                                                         GPIO_PORTD_DATA_R=(GPIO_PORTD_DATA_R | pins_mask);
+						        break;
+					       
+			   }
+			   break;
+		   }
                                  
 	               case PORTE:
+		   {
                              switch(pins_level):
+			   {
                                                case STD_LOW:
+				     
                                                         GPIO_PORTE_DATA_R=(GPIO_PORTE_DATA_R &~ pins_mask);
+						        break;
+					     
                                                case STD_HIGH:
+				              
                                                         GPIO_PORTE_DATA_R=(GPIO_PORTE_DATA_R | pins_mask);
+						        break;
+					      
+			   }
+			   break;
+		   }
                                    
 	
 	               case PORTF:
+		   {
                              switch(pins_level):
+			   {
                                                case STD_LOW:
+		   
                                                         GPIO_PORTF_DATA_R=(GPIO_PORTF_DATA_R &~ pins_mask);
+		                                        break;
+		   
                                                case STD_HIGH:
+		   
                                                         GPIO_PORTF_DATA_R=(GPIO_PORTF_DATA_R | pins_mask);
+		                                        break;
+			   }
+			   break;
+		   }
 
 }                 
