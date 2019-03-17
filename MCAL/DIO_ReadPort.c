fb0b0bbc982690006042f775821dfd2d870a5018
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-03-17 02:17:46 -0700

    Update DIO_ReadPort.c

--- MCAL/DIO_ReadPort.c
+++ MCAL/DIO_ReadPort.c
@@ -9,37 +9,35 @@ uint8 DIO_ReadPort( uint8 port_index , uint8 pins_mask )
    {
 
 	case PORTA:
-	{
+		   
                  pins_level=(GPIO_PORTA_DATA_R & pins_mask);
                  break;
-	}
 
 	case PORTB:
-	{
+		   
                  pins_level=(GPIO_PORTB_DATA_R & pins_mask);
                  break;
-	}
 			
 	case PORTC:
-	{
+
                  pins_level=(GPIO_PORTC_DATA_R & pins_mask);
                  break;
-	}
+
 	case PORTD:
-	{
+		   
                  pins_level=(GPIO_PORTD_DATA_R & pins_mask);
                  break;
-	}
+
 	case PORTE: 
-	{
+		   
                  pins_level=(GPIO_PORTE_DATA_R & pins_mask);
                  break;
-	}
+
 	case PORTF:
-	{
+		   
                  pins_level=(GPIO_PORTF_DATA_R & pins_mask);
                  break;
-	}
+
    }
   return pins_level;
 }                 
