45d4e189167ae0684c6a499ca0724279cb0e17e8
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-03-17 01:53:02 -0700

    Update DIO_ReadPort.c

--- MCAL/DIO_ReadPort.c
+++ MCAL/DIO_ReadPort.c
@@ -2,34 +2,44 @@
 #include "tm4c123gh6pm.h"
 #include "MCAL.h"
 
-uint8 DIO_ReadPort(uint8 port_index , uint8 pins_mask )
+uint8 DIO_ReadPort( uint8 port_index , uint8 pins_mask )
 {
    uint8 pins_level;
    switch(port_index){
 
 	case PORTA:
+	{
                  pins_level=(GPIO_PORTA_DATA_R & pins_mask);
-                 return pins_level;
+                 break;
+	}
 
 	case PORTB:
+	{
                  pins_level=(GPIO_PORTB_DATA_R & pins_mask);
-                 return pins_level;
+                 break;
+	}
 			
 	case PORTC:
+	{
                  pins_level=(GPIO_PORTC_DATA_R & pins_mask);
-                 return pins_level;
-
+                 break;
+	}
 	case PORTD:
+	{
                  pins_level=(GPIO_PORTD_DATA_R & pins_mask);
-                 return pins_level;
-
-	case PORTE:                 
+                 break;
+	}
+	case PORTE: 
+	{
                  pins_level=(GPIO_PORTE_DATA_R & pins_mask);
-                 return pins_level;
-
+                 break;
+	}
 	case PORTF:
+	{
                  pins_level=(GPIO_PORTF_DATA_R & pins_mask);
-                 return pins_level;
+                 break;
+	}
+  return pins_level;
 }                 
                  
    
