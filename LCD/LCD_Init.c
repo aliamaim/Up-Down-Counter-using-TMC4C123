e75d8fac4921a3a8ba4bf3d2e713c0562a21d0d8
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-03-18 12:08:51 -0700

    Update LCD_Init.c

--- LCD/LCD_Init.c
+++ LCD/LCD_Init.c
@@ -1,19 +1,19 @@
 void LCD_Init()
 {
     Port_Init(LCD_CTRL_PORT); //initialize PORTB itself in order to assign the AFSEL and Clock and other relative registers
-    Port_Init(LCD_DATA_PORT);//initialize PORTA
+    Port_Init(LCD_DATA_PORT); //initialize PORTA
     
     Port_SetPinDirection(LCD_CTRL_PORT , CTRL_MASK , PORT_PIN_OUT); //set the first 3 pins of PORTB to OUTPUT
     
-    #ifdef LCD_8BIT_MODE 
+    #ifdef LCD_8BIT_MODE //use 8 bit mode and 2 lines
           Port_SetPinDirection(LCD_DATA_PORT , DATA_8BIT_MASK , PORT_PIN_OUT);
           LCD_Command(LCD_8bit_2line_Mode);
-    #else
+    #else //use 4 bit mode and 2 lines
           Port_SetPinDirection(LCD_DATA_PORT , DATA_4BIT_MASK , PORT_PIN_OUT);
           LCD_Command(LCD_4bit_2line_Mode);
     #endif
-          LCD_Command(LCD_Clear_Display);
-          LCD_Command(LCD_Display_On_Cursor_Blink);
+          LCD_Command(LCD_Clear_Display); //give 0 to command to give clear command
+          LCD_Command(LCD_Display_On_Cursor_Blink);//let cursor blink 
  }
     
     
