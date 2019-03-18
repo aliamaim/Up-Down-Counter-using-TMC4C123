6f88f8aa348f852873ddac1f1590daa57049471f
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-03-18 14:57:38 -0700

    Update Update_LCD.c

--- Application/Update_LCD.c
+++ Application/Update_LCD.c
@@ -4,8 +4,8 @@
 extern uint32_t counter_g
 void Update_LCD()
 {
-     LCD_GoToRowCol(0,0);
-     LCD_Puts("Counter Value:");
+     LCD_GoToRowCol(0,0); //every iteration the cursor go to the first place to print our text to prevet overwrite
+     LCD_Puts("Counter Value:"); 
      LCD_GoToRowCol(1,3);
      LCD_PutsInt(counter_g);
 }
