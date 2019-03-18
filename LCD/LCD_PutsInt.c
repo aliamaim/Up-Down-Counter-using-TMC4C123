eff63b15d475a2a2cd01e25aaecbd9c5cbc04004
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2019-03-18 21:29:42 +0200

    Update LCD_PutsInt.c

--- LCD/LCD_PutsInt.c
+++ LCD/LCD_PutsInt.c
@@ -1,8 +1,8 @@
 #include "MCAL.h"
 #include <stdint.h>
 
-void LCD_PutsInt(int32_t num){
-	char buffer[8];
-	itoa(num,buffer,10);
-	LCD_Puts(buffer);
+void LCD_PutsInt(int32_t num){  //fn puts numbers on LCD as characters
+	char buffer[8];  //array to save values of strings
+	itoa(num,buffer,10); //fn convert int to strings
+	LCD_Puts(buffer); 
 }
