bbe5e7212467480921c9ca787726863a7e750721
Hamed Abdelreheem Fathallah <Hamed_fathallah@yahoo.com>
2019-03-18 18:16:35 +0200

    Update Port_SetPinPullUp.c

--- MCAL/Port_SetPinPullUp.c
+++ MCAL/Port_SetPinPullUp.c
@@ -7,19 +7,32 @@ void Port_SetPinPullUp(uint8 port_index, uint8 pins_mask, uint8 enable ){
 	if(enable==1){
 		
 		switch(port_index){
-			case PORTA:GPIO_PORTA_PUR_R |= pins_mask;
+			case PORTA:{ GPIO_PORTA_PUR_R |= pins_mask;
+				     break;
+				   }
 			
-			case PORTB:GPIO_PORTB_PUR_R |= pins_mask;
+			case PORTB:{ GPIO_PORTB_PUR_R |= pins_mask;
+				    break;
+				   }
 			
-			case PORTC:GPIO_PORTC_PUR_R |= pins_mask;
+			case PORTC:{ GPIO_PORTC_PUR_R |= pins_mask;
+				     break;
+			           }
 			
-			case PORTD:GPIO_PORTD_PUR_R |= pins_mask;
+			case PORTD:{ GPIO_PORTD_PUR_R |= pins_mask;
+				    break;
+				   }
 			
-			case PORTE:GPIO_PORTE_PUR_R |= pins_mask;
+			case PORTE:{ GPIO_PORTE_PUR_R |= pins_mask;
+				    break;
+				   }
 			
-			case PORTF:GPIO_PORTF_PUR_R |= pins_mask;
+			case PORTF:{GPIO_PORTF_PUR_R |= pins_mask;
+				    break;
+				   }
 			
-		}
-	}
+		               }
+	            
+	                 }
 	
 }
