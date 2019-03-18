04e3e196cccb2aa6c3c951c9e2eb429c20374124
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-03-18 12:09:31 -0700

    Update LCD_Init.c

--- LCD/LCD_Init.c
+++ LCD/LCD_Init.c
@@ -9,7 +9,7 @@ void LCD_Init()
           Port_SetPinDirection(LCD_DATA_PORT , DATA_8BIT_MASK , PORT_PIN_OUT);
           LCD_Command(LCD_8bit_2line_Mode);
     #else //use 4 bit mode and 2 lines
-          Port_SetPinDirection(LCD_DATA_PORT , DATA_4BIT_MASK , PORT_PIN_OUT);
+          Port_SetPinDirection(LCD_DATA_PORT , DATA_4BIT_MASK , PORT_PIN_OUT); //let 4 bits only outputs
           LCD_Command(LCD_4bit_2line_Mode);
     #endif
           LCD_Command(LCD_Clear_Display); //give 0 to command to give clear command
