8ba335ba29b6e4cd9aaead2b182be821aad032eb
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-03-18 20:03:55 +0200

    Create Systick_wait().c

---
+++ delay/Systick_wait().c
@@ -0,0 +1,10 @@
+#include "std_types.h"
+#include "tm4c123gh6pm.h"
+
+void Systick_wait(uint32_t delay){
+	
+	NVIC_ST_RELOAD_R = delay-1;
+	NVIC_ST_CURRENT_R = 0;
+	while((NVIC_ST_CTRL_R &0x00010000)==0){}
+
+}
