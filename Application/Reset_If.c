370626a55ba64f968a88fc9959d295a5110d9498
josephfarahat <36537848+josephfarahat@users.noreply.github.com>
2019-03-18 22:10:14 +0200

    Update Reset_If.c

--- Application/Reset_If.c
+++ Application/Reset_If.c
@@ -1,15 +1,15 @@
-#include<stdint.h>
+#include <stdint.h>
 #include "app.h"
 
 extern uint32_t counter_g;    
-void Reset_If()(uint8_t port_index ,uint8_t pin)   
+void Reset_If(uint8_t port_index ,uint8_t pin)   
 {
-	int m=1;
+	int m = 1;
 	while(Is_Pressed(port_index , pin))  //tool manta dayes 3ala button
 	{
-m=0;
-
+		m = 0;
 	}
-	if(m==0){counter_g=0;}
+	if(m == 0)
+		counter_g = 0;
 
 	}
