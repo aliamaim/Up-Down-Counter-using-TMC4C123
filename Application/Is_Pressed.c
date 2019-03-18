f5f265d50011869f6e01b22c39e05f4f33cd4d17
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-03-18 21:18:53 +0200

    Create Is_Pressed.c

---
+++ Application/Is_Pressed.c
@@ -0,0 +1,25 @@
+#include "MCAL.h"
+#include <stdint.h>
+#include "LCD.h"
+#include "tm4c123gh6pm.h"
+
+uint8 Is_Pressed(uint8 port_index,uint8 pin){
+	
+	uint8 state ;
+	uint8 flag = 0 ;
+	State = DIO_ReadPort(port_index,pin);
+	if(state!=pin){ // check if button is pressed 
+		
+		_delay_ms(30); // wait 30 seconds
+		
+		if(state!=pin){//check again
+			flag = 1;
+			return flag;
+		
+		}
+			
+	}
+	flag = 0;
+	return flag;
+		
+}
