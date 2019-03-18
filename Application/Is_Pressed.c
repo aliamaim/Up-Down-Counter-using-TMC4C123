fc5443722a5cc9899576b3f50d7349e48258e3f5
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-03-18 21:38:50 +0200

    Update Is_Pressed.c

--- Application/Is_Pressed.c
+++ Application/Is_Pressed.c
@@ -4,14 +4,14 @@
 #include "delay.h"
 extern uint32_t counter_g;
 
-uint8 Is_Pressed(uint8 port_index,uint8 pin){
+uint8 Is_Pressed(uint8_t port_index,uint8_t pin){
 	
-	uint8 state ;
-	uint8 flag = 0 ;
+	uint8_t state ;
+	uint8_t flag = 0 ;
 	State = DIO_ReadPort(port_index,pin);
 	if(state!=pin){ // check if button is pressed 
 		
-		_delay_ms(30); // wait 30 seconds
+		_delay_ms(30); // wait 30 milliseconds
 		
 		if(state!=pin){//check again
 			flag = 1;
