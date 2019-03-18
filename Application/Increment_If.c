372dd664ed5cf3f0ee3c6cd25cb7d27d2dac1036
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2019-03-18 22:20:31 +0200

    Update Increment_If.c

--- Application/Increment_If.c
+++ Application/Increment_If.c
@@ -1,14 +1,16 @@
 #include <stdint.h>
-#include "app.h"
 #include "delay.h"
+#include "app.h"
 
 extern uint32_t counter_g;    //global varibal (3adad)
+
 void Increment_If(uint8_t port_index ,uint8_t pin)   
 {
+	uint8_t mask = 0;
+	mask |= (pin << 1);
 	
-	while(Is_Pressed(port_index , pin))  //tool manta dayes 3ala button
+	while(Is_Pressed(port_index , mask))  //tool manta dayes 3ala button
 	{
-
 		 counter_g++;  //zawed el raqam elly hayzhar 3ala shasha
 		_delay_ms(200); //e3mel delay 0.2s bet. el increment
 	}
