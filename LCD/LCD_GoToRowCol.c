43e7275021d183041d4e6bed11002e8d716fc928
josephfarahat <36537848+josephfarahat@users.noreply.github.com>
2019-03-18 23:24:33 +0200

    Create LCD_GoToRowCol.c

---
+++ LCD/LCD_GoToRowCol.c
@@ -0,0 +1,22 @@
+
+#include <stdint.h>
+#include "LCD.h"
+
+
+void LCD_GoToRowCol(uint8_t row, uint8_t col)
+{
+	uint8_t address;
+	/*Calculate the address location*/
+	switch(row)
+	{
+	case 0:
+		address = col;
+		break;
+	case 1:
+		address = col + 0x40;
+		break;
+	}
+	/* to write to a specific address in the LCD
+	 * we need to apply the corresponding command 0b10000000+Address */
+	LCD_Command(address | LCD_Set_Cursor_Location);
+}
