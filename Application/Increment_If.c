77b3d65fcb6cb6cb79b19caa96dbd3e894d53fc6
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2019-03-19 11:50:31 +0200

    Update Increment_If.c

--- Application/Increment_If.c
+++ Application/Increment_If.c
@@ -12,6 +12,6 @@ void Increment_If(uint8_t port_index ,uint8_t pin)
 	while(Is_Pressed(port_index , mask))  //tool manta dayes 3ala button
 	{
 		 counter_g++;  //zawed el raqam elly hayzhar 3ala shasha
-		_delay_ms(200); //e3mel delay 0.2s bet. el increment
+		_delay_ms(200); //e3mel delay 0.2s bet. kol increment
 	}
 }
