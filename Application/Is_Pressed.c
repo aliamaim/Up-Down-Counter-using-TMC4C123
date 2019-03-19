c377fb6ab7aa4fae6e2c5039241228146065b583
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-03-19 04:24:49 +0200

    Update Is_Pressed.c

--- Application/Is_Pressed.c
+++ Application/Is_Pressed.c
@@ -3,14 +3,12 @@
 #include "MCAL.h"
 #include "delay.h"
 
-extern uint32_t counter_g;
 
 uint8_t Is_Pressed(uint8_t port_index, uint8_t mask){
 	if(DIO_ReadPort(port_index, mask)){ // check if button is pressed 
 		
 		_delay_ms(30); // wait 30 milliseconds to debounce
 		
-		//sha3'aleen pull down
 		if(DIO_ReadPort(port_index, mask)) //Check again if the button is pressed
 		{
 			return 1;
@@ -19,3 +17,4 @@ uint8_t Is_Pressed(uint8_t port_index, uint8_t mask){
 	}
 	return 0;
 }
+
