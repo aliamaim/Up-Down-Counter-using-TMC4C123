095707c6deebccf9f4cb812efa0d90cdbcfceb57
a7medfawzy97 <36432072+a7medfawzy97@users.noreply.github.com>
2019-03-19 04:11:29 +0200

    Update _delay_us.c

--- delay/_delay_us.c
+++ delay/_delay_us.c
@@ -1,12 +1,15 @@
 #include "MCAL.h"
 #include <stdint.h>
 #include "LCD.h"
-#include "delay.h"
+#include "tm4c123gh6pm_custom.h"
+#include  "delay.h"
 
 void _delay_us(uint32_t delay){
 	
-	uint32_t i;
-	for(i = 0; i<delay; i++){//delay*1usec
+	int i;
+	for(i = 0; i < delay; i++)
+	{
+		//delay*1usec
 		Systick_Wait(80);//1usec
 	}
 }
