917d2f15f109a6fd835ccec1ec1af7b64c23e8dc
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2019-03-19 11:49:21 +0200

    Update LCD_PutsInt.c

--- LCD/LCD_PutsInt.c
+++ LCD/LCD_PutsInt.c
@@ -1,9 +1,11 @@
-#include "MCAL.h"
-#include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
+#include "MCAL.h"
+#include "LCD.h"
 
-void LCD_PutsInt(int32_t num){  //fn puts numbers on LCD as characters
-	char buffer[8];  //array to save values of strings
-	sprintf(buffer,"%d",num);
-	LCD_Puts(buffer); 
+
+void LCD_PutsInt(int32_t num){
+	char buffer[8];
+	sprintf(buffer,"%d", num);
+	LCD_Puts(buffer);
 }
