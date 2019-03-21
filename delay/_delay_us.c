eb9a0ddb3ac0f50bf4d6829dde460cd102f168d6
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2019-03-21 03:00:28 -0700

    Update _delay_us.c

--- delay/_delay_us.c
+++ delay/_delay_us.c
@@ -1,12 +1,12 @@
 #include "MCAL.h"
 #include <stdint.h>
-#include "LCD.h"
 #include "tm4c123gh6pm_custom.h"
 #include  "delay.h"
 
 void _delay_us(uint32_t delay){
-	
 	int i;
+	Systick_Init();
+	
 	for(i = 0; i < delay; i++)
 	{
 		//delay*1usec
